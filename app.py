61b21792400bd557ac36d56de2263cf413b8d51a
Phate334 <phate334@gmail.com>
2024-11-02 09:23:52 +0800

    [add] test run file

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 import os
+from pathlib import Path
 
 import gradio as gr
 
-def greet(name):
-    return f"Hello, {name}!"
+GGUF_PARSER_VERSION = os.getenv("GGUF_PARSER_VERSION", "v0.12.0")
+gguf_parser = Path("gguf-parser-linux-amd64")
+
+def greet(tmp):
+    # Run the gguf-parser-go binary
+    gguf_parser_output = os.popen(f"./{gguf_parser} --version").read()
+    return f"{gguf_parser_output}"
 
 iface = gr.Interface(fn=greet, inputs="text", outputs="text")
 
 if __name__ == "__main__":
+    if not gguf_parser.exists():
+        gguf_parser_url = f"https://github.com/gpustack/gguf-parser-go/releases/download/{GGUF_PARSER_VERSION}/gguf-parser-linux-amd64"
+        os.system(f"wget {gguf_parser_url}")
+        os.system(f"chmod +x {gguf_parser}")
     iface.launch()
